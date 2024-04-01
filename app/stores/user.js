b70898132963d7cdb0d25be5cda0e2415423e592
Arco Mul <me@arcomul.nl>
2024-04-01 22:17:30 +0200

    refactor: clean up user store, remove authentication related values

--- app/stores/user.js
+++ app/stores/user.js
@@ -1,24 +1,19 @@
 export const useUserStore = defineStore("userStore", {
   state: () => ({
     name: null,
-    isAuthenticated: false,
     reservations: [],
   }),
   actions: {
     login({ name } = {}) {
-      const nuxtApp = useNuxtApp();
-      this.isAuthenticated = true;
-      this.name = name || nuxtApp.$pb?.authStore.model.name;
+      const { pb } = usePocketbase();
+      this.name = name || pb.authStore.model.name;
     },
     logout() {
-      this.isAuthenticated = false;
       this.name = null;
     },
     async fetchUserReservations() {
-      const nuxtApp = useNuxtApp();
-      const reservations = await nuxtApp.$pb
-        .collection("reservations")
-        .getFullList();
+      const { pb } = usePocketbase();
+      const reservations = await pb.collection("reservations").getFullList();
       this.reservations = reservations;
     },
   },
