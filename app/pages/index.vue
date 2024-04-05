22295325e01842ad76c62444e5d264901fecf1b7
Arco Mul <arco.mul@denkwerk.com>
2024-04-05 16:08:13 +0200

    feat(location): move location page to correct url

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,59 +1,6 @@
-<template>
-  <Container max-width centered>
-    <header>
-      <AddressBox class="address">{{ location?.address }}</AddressBox>
-      <h1>
-        {{ location?.name || "No location found" }}
-      </h1>
-      <div
-        class="description"
-        v-html="location?.description || 'No location found'"
-      ></div>
-    </header>
-    <hr />
-    <section>
-      <ProductGrid :location="location" />
-    </section>
-  </Container>
-</template>
+<template><h1>eh</h1></template>
 
 <script setup>
-import ProductGrid from "~/components/modules/ProductGrid.vue";
-
-const { pb } = usePocketbase();
-
-const locationId = "1351z318f7ehd9n";
-
-const { data: location } = await useAsyncData("location", async () => {
-  const location = await pb.collection("location").getOne(locationId);
-
-  return structuredClone(location);
-});
-
-useHead({
-  title: `${location.value?.name} | Leihapp`,
-});
+const router = useRouter();
+router.push("/l/leihbar-koeln-mullheim");
 </script>
-
-<style lang="scss" scoped>
-section {
-  margin-bottom: var(--fluid-spacing-8);
-}
-header {
-  .address {
-    margin-bottom: var(--fluid-spacing-8);
-  }
-  h1 {
-    margin-bottom: var(--fluid-spacing-8);
-    display: flex;
-    align-items: center;
-    line-height: 1.15;
-  }
-  .description {
-    max-width: var(--max-text-width);
-  }
-}
-h2 {
-  margin-bottom: 1rem;
-}
-</style>
---
+++ app/pages/l/[location]/index.vue
@@ -0,0 +1,62 @@
+<template>
+  <Container max-width centered>
+    <header>
+      <AddressBox class="address">{{ location?.address }}</AddressBox>
+      <h1>
+        {{ location?.name || "No location found" }}
+      </h1>
+      <div
+        class="description"
+        v-html="location?.description || 'No location found'"
+      ></div>
+    </header>
+    <hr />
+    <section>
+      <ProductGrid :location="location" />
+    </section>
+  </Container>
+</template>
+
+<script setup>
+import ProductGrid from "~/components/modules/ProductGrid.vue";
+
+const { pb } = usePocketbase();
+const route = useRoute();
+
+const slug = route.params.location;
+
+const { data: location } = await useAsyncData("location", async () => {
+  const location = await pb
+    .collection("location")
+    .getFirstListItem(pb.filter("slug = {:slug}", { slug }));
+
+  return structuredClone(location);
+});
+
+useHead({
+  title: `${location.value?.name} | Leihapp`,
+});
+</script>
+
+<style lang="scss" scoped>
+section {
+  margin-bottom: var(--fluid-spacing-8);
+}
+header {
+  .address {
+    margin-bottom: var(--fluid-spacing-8);
+  }
+  h1 {
+    margin-bottom: var(--fluid-spacing-8);
+    display: flex;
+    align-items: center;
+    line-height: 1.15;
+  }
+  .description {
+    max-width: var(--max-text-width);
+  }
+}
+h2 {
+  margin-bottom: 1rem;
+}
+</style>
