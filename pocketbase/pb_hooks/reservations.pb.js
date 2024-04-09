5a196fe5857e6bdac5784d8c7a7f79a55a75e552
Arco Mul <me@arcomul.nl>
2024-04-09 22:13:24 +0200

    feat(reservation): validate if there is no overlapping reservation in pocketbase hook

--- pocketbase/pb_hooks/reservations.pb.js
+++ pocketbase/pb_hooks/reservations.pb.js
@@ -1,14 +1,19 @@
 onRecordBeforeCreateRequest((e) => {
-  console.log(JSON.stringify(e.httpContext, null, 2));
-  console.log(JSON.stringify(e.record, null, 2));
-
-  const records = $app.dao().findRecordsByFilter(
-    "public_reservations", // collection
-    "product = {:product}", // filter
-    "-id", // sort
-    5, // limit
-    0, // offset
-    { product: e.record.product } // optional filter params
-  );
-  console.log(JSON.stringify(records, null, 2));
+  const records = $app
+    .dao()
+    .findRecordsByFilter(
+      "reservations",
+      "product = {:product} && start <= {:end} && end >= {:start}",
+      null,
+      1,
+      0,
+      {
+        product: e.record.get("product"),
+        start: e.record.get("start"),
+        end: e.record.get("end"),
+      }
+    );
+  if (records.length > 0) {
+    throw new BadRequestError("Overlapping_reservation.");
+  }
 }, "reservations");
