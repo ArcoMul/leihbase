1360cc26560b06bb3d965905abdede2ff6b52f11
Arco Mul <arco.mul@denkwerk.com>
2024-04-12 10:12:47 +0200

    feat(login): style login form in a card

--- app/pages/login.vue
+++ app/pages/login.vue
@@ -1,40 +1,44 @@
 <template>
-  <Container width="sm" centered>
-    <h1>Log in</h1>
-    <p>
-      Noch kein Konto? <NuxtLink :to="signupLink">Meldest du dich an</NuxtLink>,
-      um ein Konto zu erstellen.
-    </p>
-    <form @submit.prevent="onLogin">
-      <Input
-        label="E-mail"
-        type="email"
-        id="email"
-        name="email"
-        required
-        v-model="email"
-      />
-      <Input
-        label="Kernwort"
-        type="password"
-        id="password"
-        name="password"
-        required
-        password-toggle
-        v-model="password"
-      />
-      <p v-if="authenticationError">
-        Log in not successful, please review your account details, or
-        <NuxtLink to="/signup">sign up</NuxtLink> to create an account
+  <Container width="sm" centered no-padding>
+    <Card class="card">
+      <h1>Log in</h1>
+      <p>
+        Noch kein Konto?
+        <NuxtLink :to="signupLink">Meldest du dich an</NuxtLink>, um ein Konto
+        zu erstellen.
       </p>
-      <Button type="submit">Log in</Button>
-    </form>
+      <form @submit.prevent="onLogin">
+        <Input
+          label="E-mail"
+          type="email"
+          id="email"
+          name="email"
+          required
+          v-model="email"
+        />
+        <Input
+          label="Kernwort"
+          type="password"
+          id="password"
+          name="password"
+          required
+          password-toggle
+          v-model="password"
+        />
+        <p v-if="authenticationError">
+          Log in not successful, please review your account details, or
+          <NuxtLink to="/signup">sign up</NuxtLink> to create an account
+        </p>
+        <Button type="submit">Log in</Button>
+      </form>
+    </Card>
   </Container>
 </template>
 
 <script setup>
 import Container from "~/components/Container";
 import Input from "~/components/Input";
+import Card from "~/components/Card";
 
 const router = useRouter();
 const route = useRoute();
@@ -69,6 +73,14 @@ async function onLogin() {
 </script>
 
 <style lang="scss" scoped>
+@media (min-width: 600px) {
+  .card {
+    margin-top: 3rem;
+  }
+  h1 {
+    margin-top: -1rem;
+  }
+}
 form {
   display: flex;
   flex-direction: column;
