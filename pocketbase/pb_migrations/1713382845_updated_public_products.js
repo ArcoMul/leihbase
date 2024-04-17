cd019165fc0ba025565ecedfab5c56e4bdb03789
Arco Mul <me@arcomul.nl>
2024-04-17 22:06:25 +0200

    feat(pocketbase): define thumb sizes for product images

---
+++ pocketbase/pb_migrations/1713382845_updated_public_products.js
@@ -0,0 +1,258 @@
+/// <reference path="../pb_data/types.d.ts" />
+migrate((db) => {
+  const dao = new Dao(db)
+  const collection = dao.findCollectionByNameOrId("23lng9rcyhwckvp")
+
+  // remove
+  collection.schema.removeField("pd25zepg")
+
+  // remove
+  collection.schema.removeField("ufmmg87k")
+
+  // remove
+  collection.schema.removeField("kt522vqq")
+
+  // remove
+  collection.schema.removeField("592qgzcs")
+
+  // remove
+  collection.schema.removeField("trmhonri")
+
+  // remove
+  collection.schema.removeField("bxx0ph9q")
+
+  // add
+  collection.schema.addField(new SchemaField({
+    "system": false,
+    "id": "7073qqzh",
+    "name": "name",
+    "type": "text",
+    "required": false,
+    "presentable": false,
+    "unique": false,
+    "options": {
+      "min": null,
+      "max": null,
+      "pattern": ""
+    }
+  }))
+
+  // add
+  collection.schema.addField(new SchemaField({
+    "system": false,
+    "id": "3zqlysrx",
+    "name": "location",
+    "type": "relation",
+    "required": false,
+    "presentable": false,
+    "unique": false,
+    "options": {
+      "collectionId": "nkbfankqkyubdu9",
+      "cascadeDelete": false,
+      "minSelect": null,
+      "maxSelect": 1,
+      "displayFields": null
+    }
+  }))
+
+  // add
+  collection.schema.addField(new SchemaField({
+    "system": false,
+    "id": "ayykvskv",
+    "name": "categories",
+    "type": "relation",
+    "required": false,
+    "presentable": false,
+    "unique": false,
+    "options": {
+      "collectionId": "eq4iy7753ga1h1m",
+      "cascadeDelete": false,
+      "minSelect": null,
+      "maxSelect": null,
+      "displayFields": null
+    }
+  }))
+
+  // add
+  collection.schema.addField(new SchemaField({
+    "system": false,
+    "id": "bkim6wko",
+    "name": "images",
+    "type": "file",
+    "required": false,
+    "presentable": false,
+    "unique": false,
+    "options": {
+      "mimeTypes": [],
+      "thumbs": [
+        "600x0",
+        "1200x0"
+      ],
+      "maxSelect": 99,
+      "maxSize": 5242880,
+      "protected": false
+    }
+  }))
+
+  // add
+  collection.schema.addField(new SchemaField({
+    "system": false,
+    "id": "4am43xza",
+    "name": "description",
+    "type": "editor",
+    "required": false,
+    "presentable": false,
+    "unique": false,
+    "options": {
+      "convertUrls": false
+    }
+  }))
+
+  // add
+  collection.schema.addField(new SchemaField({
+    "system": false,
+    "id": "xbt5ntjc",
+    "name": "ongoingReservation",
+    "type": "relation",
+    "required": false,
+    "presentable": false,
+    "unique": false,
+    "options": {
+      "collectionId": "h7plyphsy0mgjpf",
+      "cascadeDelete": false,
+      "minSelect": null,
+      "maxSelect": 1,
+      "displayFields": null
+    }
+  }))
+
+  return dao.saveCollection(collection)
+}, (db) => {
+  const dao = new Dao(db)
+  const collection = dao.findCollectionByNameOrId("23lng9rcyhwckvp")
+
+  // add
+  collection.schema.addField(new SchemaField({
+    "system": false,
+    "id": "pd25zepg",
+    "name": "name",
+    "type": "text",
+    "required": false,
+    "presentable": false,
+    "unique": false,
+    "options": {
+      "min": null,
+      "max": null,
+      "pattern": ""
+    }
+  }))
+
+  // add
+  collection.schema.addField(new SchemaField({
+    "system": false,
+    "id": "ufmmg87k",
+    "name": "location",
+    "type": "relation",
+    "required": false,
+    "presentable": false,
+    "unique": false,
+    "options": {
+      "collectionId": "nkbfankqkyubdu9",
+      "cascadeDelete": false,
+      "minSelect": null,
+      "maxSelect": 1,
+      "displayFields": null
+    }
+  }))
+
+  // add
+  collection.schema.addField(new SchemaField({
+    "system": false,
+    "id": "kt522vqq",
+    "name": "categories",
+    "type": "relation",
+    "required": false,
+    "presentable": false,
+    "unique": false,
+    "options": {
+      "collectionId": "eq4iy7753ga1h1m",
+      "cascadeDelete": false,
+      "minSelect": null,
+      "maxSelect": null,
+      "displayFields": null
+    }
+  }))
+
+  // add
+  collection.schema.addField(new SchemaField({
+    "system": false,
+    "id": "592qgzcs",
+    "name": "images",
+    "type": "file",
+    "required": false,
+    "presentable": false,
+    "unique": false,
+    "options": {
+      "mimeTypes": [],
+      "thumbs": [
+        "600x0",
+        "1200x0"
+      ],
+      "maxSelect": 99,
+      "maxSize": 5242880,
+      "protected": false
+    }
+  }))
+
+  // add
+  collection.schema.addField(new SchemaField({
+    "system": false,
+    "id": "trmhonri",
+    "name": "description",
+    "type": "editor",
+    "required": false,
+    "presentable": false,
+    "unique": false,
+    "options": {
+      "convertUrls": false
+    }
+  }))
+
+  // add
+  collection.schema.addField(new SchemaField({
+    "system": false,
+    "id": "bxx0ph9q",
+    "name": "ongoingReservation",
+    "type": "relation",
+    "required": false,
+    "presentable": false,
+    "unique": false,
+    "options": {
+      "collectionId": "h7plyphsy0mgjpf",
+      "cascadeDelete": false,
+      "minSelect": null,
+      "maxSelect": 1,
+      "displayFields": null
+    }
+  }))
+
+  // remove
+  collection.schema.removeField("7073qqzh")
+
+  // remove
+  collection.schema.removeField("3zqlysrx")
+
+  // remove
+  collection.schema.removeField("ayykvskv")
+
+  // remove
+  collection.schema.removeField("bkim6wko")
+
+  // remove
+  collection.schema.removeField("4am43xza")
+
+  // remove
+  collection.schema.removeField("xbt5ntjc")
+
+  return dao.saveCollection(collection)
+})
