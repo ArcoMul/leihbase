9e1fc6f9fea044811b2d731358f50cac7b186803
Arco Mul <me@arcomul.nl>
2024-04-17 22:48:16 +0200

    feat(link): add page/route to redirect to a product page using product id

---
+++ app/pages/link/product/[id].vue
@@ -0,0 +1,14 @@
+<template></template>
+
+<script setup>
+const route = useRoute();
+const { pb } = usePocketbase();
+
+const product = await pb
+  .collection("public_products")
+  .getOne(route.params.id, { expand: "location" });
+
+await navigateTo({
+  path: `/l/${product.expand.location.slug}/p/${route.params.id}`,
+});
+</script>
