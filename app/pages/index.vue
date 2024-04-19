078c01f7b81410c07b5933f15bc1a1410e908652
Arco Mul <me@arcomul.nl>
2024-04-19 15:10:47 +0200

    feat(index): redirect to first active location

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,7 +1,18 @@
 <template></template>
 
 <script setup>
-await navigateTo({
-  path: `/l/leihbar-koeln-mullheim`,
+const { pb } = usePocketbase();
+const locations = await pb.collection("location").getFullList({
+  filter: "active = true",
 });
+if (locations.length > 0) {
+  await navigateTo({
+    path: `/l/${locations[0].slug}`,
+  });
+} else {
+  throw createError({
+    statusCode: 404,
+    statusMessage: "Page Not Found",
+  });
+}
 </script>
