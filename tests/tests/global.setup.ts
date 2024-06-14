46cd570484b0cc5652d11c7934ffdc18874e57c5
Arco Mul <me@arcomul.nl>
2024-06-14 22:24:42 +0200

    fix: wait longer for test setup

--- tests/tests/global.setup.ts
+++ tests/tests/global.setup.ts
@@ -7,27 +7,26 @@ setup.describe("setup", () => {
   setup("create location", async ({}) => {
     const pb = new PocketBase("http://127.0.0.1:8080");
 
-    // or as super-admin
-    const adminData = await pb.admins.authWithPassword(
-      "test@example.com",
-      "1234567890"
-    );
+    // Auth as admin
+    await pb.admins.authWithPassword("test@example.com", "1234567890");
 
-    // list and filter "example" collection records
+    // Create test store
     try {
-      const result = await pb.collection("location").create({
+      await pb.collection("location").create({
         active: true,
         name: "Test Store",
         address: "Example Street 1, Example City",
         slug: "test-store",
       });
     } catch (err) {
-      console.log(err.response.data);
+      console.log("Error creating test store", err.response.data);
     }
   });
 
+  // Wait for homepage to load with extra long timeout
+  // as Nuxt first needs to finish building
   setup("wait for homepage", async ({ page }) => {
     await page.goto("/");
-    await expect(page).toHaveTitle("Test Store | Leihbase");
+    await expect(page).toHaveTitle("Test Store | Leihbase", { timeout: 30000 });
   });
 });
