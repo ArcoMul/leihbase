352a2907203c72847bf92f6daa6e8c1c9cf9f8bf
Arco Mul <arco.mul@denkwerk.com>
2024-08-27 22:52:10 +0200

    feat: add Tooltip component

---
+++ app/components/Tooltip.vue
@@ -0,0 +1,30 @@
+<template>
+  <sl-tooltip :open="open" @click.prevent.stop>
+    <div slot="content" v-html="html"></div>
+    <div
+      class="trigger"
+      @click.prevent.stop="handleClick"
+      @blur="handleBlur"
+      tabindex="0"
+    >
+      <slot />
+    </div>
+  </sl-tooltip>
+</template>
+
+<script setup lang="ts">
+defineProps<{ html: string }>();
+const open = ref(false);
+function handleClick() {
+  open.value = true;
+}
+function handleBlur() {
+  open.value = false;
+}
+</script>
+
+<style scoped>
+.trigger {
+  display: inline-block;
+}
+</style>
