b82c2392d7ca4fd474c8064c22f76ee8ec090ede
Arco Mul <arco.mul@denkwerk.com>
2024-08-27 22:53:16 +0200

    feat: add started, ended and deposit fields to reservation

---
+++ pocketbase/pb_migrations/1724786860_updated_reservations.js
@@ -0,0 +1,61 @@
+/// <reference path="../pb_data/types.d.ts" />
+migrate((db) => {
+  const dao = new Dao(db)
+  const collection = dao.findCollectionByNameOrId("h7plyphsy0mgjpf")
+
+  // add
+  collection.schema.addField(new SchemaField({
+    "system": false,
+    "id": "wdukhdwl",
+    "name": "started",
+    "type": "bool",
+    "required": false,
+    "presentable": false,
+    "unique": false,
+    "options": {}
+  }))
+
+  // add
+  collection.schema.addField(new SchemaField({
+    "system": false,
+    "id": "rhcvyxrc",
+    "name": "ended",
+    "type": "bool",
+    "required": false,
+    "presentable": false,
+    "unique": false,
+    "options": {}
+  }))
+
+  // add
+  collection.schema.addField(new SchemaField({
+    "system": false,
+    "id": "yrxfsdwf",
+    "name": "deposit",
+    "type": "number",
+    "required": false,
+    "presentable": false,
+    "unique": false,
+    "options": {
+      "min": null,
+      "max": null,
+      "noDecimal": false
+    }
+  }))
+
+  return dao.saveCollection(collection)
+}, (db) => {
+  const dao = new Dao(db)
+  const collection = dao.findCollectionByNameOrId("h7plyphsy0mgjpf")
+
+  // remove
+  collection.schema.removeField("wdukhdwl")
+
+  // remove
+  collection.schema.removeField("rhcvyxrc")
+
+  // remove
+  collection.schema.removeField("yrxfsdwf")
+
+  return dao.saveCollection(collection)
+})
