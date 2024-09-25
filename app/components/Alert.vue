29391fe4defad1985b7661f45eaa6a0d137f8496
Arco Mul <arco.mul@denkwerk.com>
2024-09-25 17:26:18 +0200

    feat: add warning variant to Alert component

--- app/components/Alert.vue
+++ app/components/Alert.vue
@@ -1,13 +1,15 @@
 <template>
   <div :class="{ alert: true, [variant]: true }">
     <ThumbsUp v-if="variant === 'success'" />
+    <WarningTriangle v-else-if="variant === 'warning'" />
     <slot></slot>
   </div>
 </template>
 
 <script setup lang="ts">
 import { ThumbsUp } from "@iconoir/vue";
-withDefaults(defineProps<{ variant: "success" | "error" }>(), {
+import { WarningTriangle } from "@iconoir/vue";
+withDefaults(defineProps<{ variant: "success" | "error" | "warning" }>(), {
   variant: "success",
 });
 </script>
@@ -21,6 +23,9 @@ withDefaults(defineProps<{ variant: "success" | "error" }>(), {
   margin-bottom: 2rem;
   &.success {
     background-color: var(--background-success);
+  }
+  &.warning {
+    background-color: var(--background-warning);
   }
   &.error {
     background-color: var(--background-error);
