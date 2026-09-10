30dba92b35eabe27b6e0379dd2d309e4a7b4bfe1
Arco Mul <me@arcomul.nl>
2026-09-10 10:18:57 +0200

    fix: add size property to Drawer

--- web/app/components/core/Drawer.vue
+++ web/app/components/core/Drawer.vue
@@ -6,14 +6,23 @@
   <div
     ref="sidebar"
     data-base-theme
-    :class="{ sidebar: true, open, 'header-offset': headerOffset, inset }"
+    :class="{
+      sidebar: true,
+      open,
+      'header-offset': headerOffset,
+      inset,
+      ['size-' + size]: true,
+    }"
   >
     <slot></slot>
   </div>
 </template>
 
 <script lang="ts" setup>
-defineProps<{ headerOffset: boolean; inset: boolean }>();
+withDefaults(
+  defineProps<{ headerOffset: boolean; inset: boolean; size?: "sm" | "md" }>(),
+  { size: "sm" }
+);
 
 const open = defineModel("open");
 const sidebar = ref<HTMLDivElement>();
@@ -90,6 +99,12 @@ function handleDocumentKeyDown(e: KeyboardEvent) {
   @media screen and (min-width: breakpoints.$breakpoint-md) {
     width: 66.66%;
     max-width: 600px;
+  }
+}
+.sidebar.size-md {
+  @media screen and (min-width: breakpoints.$breakpoint-md) {
+    width: 66.66%;
+    max-width: 800px;
   }
 }
 .header-offset {
